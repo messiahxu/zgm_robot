3a74fcb3be0dcd605239808b4febe316f78b54b1
zhouguangming <zhouguangming1989@gmail.com>
2012-06-03 21:26:41 +0800

    fix session file

--- app/controllers/robots_controller.rb
+++ app/controllers/robots_controller.rb
@@ -10,13 +10,15 @@ class RobotsController < ApplicationController
 
     if session[:user].blank?
       session[:user] = request.session_options[:id]
-      $last_user = session[:user]
       ProgramR::History.init
     elsif $last_user != session[:user]
-      $last_user = session[:user]
-      ProgramR::History.loading "tmp/my_sessions/#{session[:user]}"
+      if File.exist("tmp/my_sessions/#{session[:user]}")
+        ProgramR::History.loading "tmp/my_sessions/#{session[:user]}"
+      end
+    else
+      p 'welcome back'
     end
-
+    $last_user = session[:user]
     if receive.blank?
       @reply = NullReply
     elsif receive =~ /[\u4e00-\u9fa5]/
