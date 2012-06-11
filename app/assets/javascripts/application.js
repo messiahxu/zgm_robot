5cc9f89aa6ed8104e8334b4b5708bc55a0e4c3f3
zhouguangming <zhouguangming1989@gmail.com>
2012-06-11 14:21:03 +0800

    fix javascript alert bug

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,7 +26,6 @@ $(document).ready(function(){
       $('#receive').attr('readonly', true); 
       $('#receive').attr('value', 'Give me 10 seconds, be patient...'); 
       receive = receive.replace(/\+/g, "%2B")
-    alert(receive);
       $.ajax({
         url:'/robots/chat?receive='+escape(receive),
         success:function(){
